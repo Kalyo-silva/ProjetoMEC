0318e15a9885e97619918c4c32ba4d1265a9a870
kalyo-silva <kalyo.silva@unidavi.edu.br>
2025-10-09 16:37:35 -0300

    Ajuste na interface

--- MEC/resources/views/instituicoes/index.blade.php
+++ MEC/resources/views/instituicoes/index.blade.php
@@ -23,27 +23,48 @@
                     @if($listaInstituicoes->isEmpty())
                         <p>Nenhuma Instituição Encontrado.</p>
                     @else
-                        <div class="grid grid-cols-2 gap-4">
+                        <div id="container_inst" class="grid grid-cols-2 gap-4">
                             @foreach ($listaInstituicoes as $inst)
-                                <div class="border rounded p-4 flex items-center gap-4 mb-2 justify-between">
-                                    <div class="flex justify-between w-full">
-                                        <div class="flex items-center gap-4">
-                                            <div>
-                                                <img class="size-16 object-cover border rounded" src="{{asset('img_instituicoes/'.$inst->logo)}}" alt="logo">
+                                <div id="inst_{{$inst->id}}" class="border transition-all rounded p-4 flex items-center gap-4 mb-2 justify-between">
+                                    <div class="flex flex-col w-full">
+                                        <div class="flex justify-between w-full">
+                                            <div class="flex items-center gap-4">
+                                                <div>
+                                                    <img class="size-16 transition-all object-cover border rounded" src="{{asset('img_instituicoes/'.$inst->logo)}}" alt="logo">
+                                                </div>
+                                                <div>
+                                                    <div>
+                                                        <p class="text-indigo-400 font-bold label hidden">Nome</p>
+                                                        <h2 class="font-bold">{{$inst->nome}}</h2>
+                                                        <p class="text-indigo-400 font-bold label hidden">Sigla</p>
+                                                        <p class="italic text-gray-500">{{$inst->sigla}}</p>
+                                                    </div>
+                                                    <div class="hidden info">
+                                                        <p class="text-indigo-400 font-bold">Endereço</p>
+                                                        <p>{{$inst->logradouro}} - {{$inst->bairro}}, {{$inst->cidade}} - {{$inst->uf}} - {{substr_replace($inst->cep, '-', 5, 0)}}</p>
+                                                    </div>
+                                                    <div class="justify-between hidden info">
+                                                        <div>
+                                                        <p class="text-indigo-400 font-bold">Mantenedor</p>
+
+                                                        <a href="{{route('mantenedores.index')}}" class="flex items-center gap-1 border rounded w-fit px-4 py-2 cursor-pointer">
+                                                            <x-uni-university-o class="size-4 text-indigo-400"/>
+                                                            <p class="">{{$inst->mantenedor->nome}}</p>
+                                                        </a>
+                                                        </div>
+                                                    </div>
+                                                </div>
                                             </div>
-                                            <div>
-                                                <h2 class="font-bold">{{$inst->nome}}</h2>
-                                                <p class="italic text-gray-500">{{$inst->sigla}}</p>
+                                            <div class="flex gap-2 items-start  ">
+                                                <div class="hidden options">
+                                                    <x-eva-trash-outline class="size-6 text-gray-300 hover:text-gray-700 hover:cursor-pointer"/>
+                                                    <a href="{{route('instituicoes.edit',$inst->id)}}">
+                                                        <x-eva-edit-outline class="size-6 text-gray-300 hover:text-gray-700 hover:cursor-pointer"/>
+                                                    </a>
+                                                </div>
+
+                                                <x-eva-expand onclick="detail({{$inst->id}})" class="size-6 text-gray-300 hover:text-gray-700 hover:cursor-pointer"/>
                                             </div>
-                                        </div>
-                                        <div class="flex gap-2 items-center">
-                                            <a href="{{route('instituicoes.show', $inst->id)}}">
-                                                <x-eva-search-outline class="size-6 text-gray-300 hover:text-gray-700 hover:cursor-pointer"/>
-                                            </a>
-                                            <a href="{{route('instituicoes.edit',$inst->id)}}">
-                                                <x-eva-edit-outline class="size-6 text-gray-300 hover:text-gray-700 hover:cursor-pointer"/>
-                                            </a>
-                                            <x-eva-trash-outline class="size-6 text-gray-300 hover:text-gray-700 hover:cursor-pointer"/>
                                         </div>
                                     </div>
                                 </div>
@@ -59,3 +80,47 @@
         </div>
     </div>
 </x-app-layout>
+
+<script>
+    function detail(id){
+        const container = document.getElementById('inst_'+id);
+        const img = container.querySelectorAll("img");
+        const labels = container.querySelectorAll('p.label');
+        const info = container.querySelectorAll('div.info');
+        const options = container.querySelectorAll('div.options');
+
+        if (container.className.includes(' col-span-2 ')){
+            container.className = container.className.replace(' col-span-2 ', '')
+            img[0].className = img[0].className.replace('size-64', 'size-16')
+
+            for (let i = 0; i < labels.length; i++) {
+                labels[i].className += "hidden"
+            }
+
+            for (let i = 0; i < info.length; i++) {
+                info[i].className = info[i].className.replace("flex flex-col", "hidden")
+            }
+
+            for (let i = 0; i < options.length; i++) {
+                options[i].className = options[i].className.replace("flex gap-2","hidden")
+            }
+        }
+        else{
+            container.className += ' col-span-2 '
+            img[0].className = img[0].className.replace('size-16', 'size-64')
+
+            for (let i = 0; i < labels.length; i++) {
+                labels[i].className = labels[i].className.replace("hidden", "")
+            }
+
+            for (let i = 0; i < info.length; i++) {
+                info[i].className = info[i].className.replace("hidden", "flex flex-col")
+            }
+
+            for (let i = 0; i < options.length; i++) {
+                options[i].className = options[i].className.replace("hidden", "flex gap-2")
+            }
+        }
+
+    }
+</script>
--- MEC/resources/views/mantenedor/index.blade.php
+++ MEC/resources/views/mantenedor/index.blade.php
@@ -12,31 +12,39 @@
                     <div class="flex justify-between items-center">
 
                         <div class="flex gap-1 items-center">
-                            <x-uni-university-o class="size-6 text-gray-400"/>
+                            <x-uni-university-o class="size-6 text-gray-400" />
                             <h2 class='text-xl border-b-2 border-indigo-400 font-bold'>Lista de Mantenedores</h2>
                         </div>
                         <div class="linkButton">
-                            <x-fas-plus class="size-6"/>
-                            <a href="{{route('mantenedores.create')}}">Novo</a>
+                            <x-fas-plus class="size-6" />
+                            <a href="{{ route('mantenedores.create') }}">Novo</a>
                         </div>
                     </div>
 
-                    @if($listaMantenedores->isEmpty())
+                    @if ($listaMantenedores->isEmpty())
                         <p>Nenhum Mantenedor Encontrado.</p>
                     @else
                         @foreach ($listaMantenedores as $mantenedor)
                             <div class="rounded border mt-4 px-4 py-4 shadow-lg">
                                 <div class="flex justify-between items-center">
                                     <div class="flex items-center">
-                                        <x-uni-university-o class="size-6 mr-2 text-gray-500"/>
-                                        <h2 class="font-bold">{{$mantenedor->nome}}</h2>
+                                        <x-uni-university-o class="size-6 mr-2 text-gray-500" />
+                                        <h2 class="font-bold">{{ $mantenedor->nome }}</h2>
                                     </div>
                                     <div class="flex items-center">
-                                        <p class="text-gray-700 mr-4">@if(!empty($mantenedor->instituicao)) {{count($mantenedor->instituicao)}} @else {{0}} @endif Instituições</p>
-                                        <x-eva-arrow-down-outline class="size-8 text-gray-500 cursor-pointer transition-all" onclick="OpenMenuDetalhes(this,'details_mantenedor_{{$mantenedor->id}}')"/>
+                                        <p class="text-gray-700 mr-4">
+                                            @if (!empty($mantenedor->instituicao))
+                                                {{ count($mantenedor->instituicao) }}
+                                            @else
+                                                {{ 0 }}
+                                            @endif Instituições
+                                        </p>
+                                        <x-eva-arrow-down-outline
+                                            class="size-8 text-gray-500 cursor-pointer transition-all"
+                                            onclick="OpenMenuDetalhes(this,'details_mantenedor_{{ $mantenedor->id }}')" />
                                     </div>
                                 </div>
-                                <div id='details_mantenedor_{{$mantenedor->id}}' class="hidden gap-4 mt-4">
+                                <div id='details_mantenedor_{{ $mantenedor->id }}' class="hidden gap-4 mt-4">
                                     <div class="flex gap-4">
                                         <div class="shadow-lg border rounded p-4 w-1/2 h-full">
                                             <h2 class="font-bold border-b-2 w-fit border-indigo-400 mb-2">Endereço</h2>
@@ -44,71 +52,81 @@
                                             <div class="flex justify-between items-center mb-2">
                                                 <div class='labeledInput w-10/12 mr-4'>
                                                     <label for='cidade'>Cidade</label>
-                                                    <input name='cidade' id='cidade' type='text' value="{{$mantenedor->cidade}}" readonly>
+                                                    <input name='cidade' id='cidade' type='text'
+                                                        value="{{ $mantenedor->cidade }}" readonly>
                                                 </div>
 
                                                 <div class='labeledInput w-2/12'>
                                                     <label for='uf'>UF</label>
-                                                    <input name='uf' id='uf' type='text' value="{{$mantenedor->uf}}" readonly>
+                                                    <input name='uf' id='uf' type='text'
+                                                        value="{{ $mantenedor->uf }}" readonly>
                                                 </div>
                                             </div>
 
                                             <div class="flex justify-between items-center mb-2">
                                                 <div class='labeledInput w-1/2 mr-4'>
                                                     <label for='bairro'>Bairro</label>
-                                                    <input name='bairro' id='bairro' type='text' value="{{$mantenedor->bairro}}" readonly>
+                                                    <input name='bairro' id='bairro' type='text'
+                                                        value="{{ $mantenedor->bairro }}" readonly>
                                                 </div>
 
                                                 <div class='labeledInput w-1/2'>
                                                     <label for='cep'>CEP</label>
-                                                    <input name='cep' id='cep' type='text' value="{{$mantenedor->cep}}" readonly>
+                                                    <input name='cep' id='cep' type='text'
+                                                        value="{{ $mantenedor->cep }}" readonly>
                                                 </div>
                                             </div>
                                             <div class='labeledInput'>
                                                 <label for='logradouro'>Logradouro</label>
-                                                <input name='logradouro' id='logradouro' type='text' value="{{$mantenedor->logradouro}}" readonly>
+                                                <input name='logradouro' id='logradouro' type='text'
+                                                    value="{{ $mantenedor->logradouro }}" readonly>
                                             </div>
 
                                         </div>
                                         <div class="shadow-lg border rounded p-4 w-1/2">
-                                            <h2 class="font-bold border-b-2 w-fit border-indigo-400 mb-2">Instituições Vinculadas</h2>
+                                            <h2 class="font-bold border-b-2 w-fit border-indigo-400 mb-2">Instituições
+                                                Vinculadas</h2>
 
-                                                @if (count($mantenedor->instituicao) != 0)
-                                                    <div class="max-h-64 overflow-y-scroll">
-                                                        @foreach ($mantenedor->instituicao as $inst)
-                                                            <div class="flex gap-4 items-center mb-2 py-2 px-4 border rounded justify-between">
-                                                                <div class="flex gap-4 items-center">
-                                                                    <div>
-                                                                        <img class='size-12 rounded object-cover border shadow-lg' src="{{asset('img_instituicoes/'.$inst->logo)}}" alt="logo">
-                                                                    </div>
-                                                                    <div>
-                                                                        <h2 class="font-bold">{{$inst->nome}}</h2>
-                                                                        <p class="text-sm">{{$inst->sigla}}</p>
-                                                                    </div>
+                                            @if (count($mantenedor->instituicao) != 0)
+                                                <div class="max-h-64 overflow-y-scroll">
+                                                    @foreach ($mantenedor->instituicao as $inst)
+                                                        <div
+                                                            class="flex gap-4 items-center mb-2 py-2 px-4 border rounded justify-between">
+                                                            <div class="flex gap-4 items-center">
+                                                                <div>
+                                                                    <img class='size-12 rounded object-cover border shadow-lg'
+                                                                        src="{{ asset('img_instituicoes/' . $inst->logo) }}"
+                                                                        alt="logo">
                                                                 </div>
-                                                                <a href="{{route('instituicoes.show', $inst->id)}}">
-                                                                    <x-eva-search-outline class="size-6 text-gray-300 hover:text-gray-700 hover:cursor-pointer"/>
-                                                                </a>
+                                                                <div>
+                                                                    <h2 class="font-bold">{{ $inst->nome }}</h2>
+                                                                    <p class="text-sm">{{ $inst->sigla }}</p>
+                                                                </div>
                                                             </div>
-                                                        @endforeach
-                                                    </div>
-                                                @else
-                                                    <div class="flex justify-center items-center h-5/6">
-                                                        <p class="text-gray-500">Nenhuma Instituição vinculada... 😕</p>
-                                                    </div>
-                                                @endif
+                                                            <a href="{{ route('instituicoes.show', $inst->id) }}">
+                                                                <x-eva-search-outline
+                                                                    class="size-6 text-gray-300 hover:text-gray-700 hover:cursor-pointer" />
+                                                            </a>
+                                                        </div>
+                                                    @endforeach
+                                                </div>
+                                            @else
+                                                <div class="flex justify-center items-center h-5/6">
+                                                    <p class="text-gray-500">Nenhuma Instituição vinculada... 😕</p>
+                                                </div>
+                                            @endif
                                         </div>
                                     </div>
                                     <div class="flex mt-4 flex-row-reverse gap-4">
                                         <div class="linkButton hover:text-indigo-400 hover:border-indigo-400">
-                                            <a href="{{route('mantenedores.edit',$mantenedor->id)}}" class="flex">
-                                                <x-eva-edit-outline class="size-6"/>
+                                            <a href="{{ route('mantenedores.edit', $mantenedor->id) }}" class="flex">
+                                                <x-eva-edit-outline class="size-6" />
                                                 <p>Editar</p>
                                             </a>
                                         </div>
                                         <div class="linkButton hover:text-red-500 hover:border-red-500">
-                                            <a href="{{route('mantenedores.edit',$mantenedor->id)}}" class="flex">
-                                                <x-eva-trash-outline class="size-6"/>
+                                            <a href="{{ route('mantenedores.edit', $mantenedor->id) }}" class="flex">
+                                                <x-eva-trash-outline class="size-6" />
                                                 <p>Excluir</p>
                                             </a>
                                         </div>
@@ -119,7 +137,7 @@
                     @endif
 
                     <div class="mt-4">
-                        {{$listaMantenedores->links()}}
+                        {{ $listaMantenedores->links() }}
                     </div>
                 </div>
             </div>
@@ -128,14 +146,13 @@
 </x-app-layout>
 
 <script>
-    function OpenMenuDetalhes(button, id){
+    function OpenMenuDetalhes(button, id) {
         var menu = document.getElementById(id);
 
-        if (menu.style.display != 'none' && menu.style.display != ''){
+        if (menu.style.display != 'none' && menu.style.display != '') {
             menu.style.display = 'none';
             button.style = 'transform : rotate(0deg);';
-        }
-        else{
+        } else {
             menu.style.display = 'block';
             button.style = 'transform : rotate(180deg);';
         }
